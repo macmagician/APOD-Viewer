9cd01fe5badcc9f9bba63925373cc60d6e397db6
Adam Gerber <adamgerber@coracle.ltd.uk>
2025-07-01 20:11:36 +0100

    Initial Commit

---
+++ APOD Viewer/APOD_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  APOD_ViewerApp.swift
+//  APOD Viewer
+//
+//  Created by Adam Gerber on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct APOD_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
